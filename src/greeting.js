98da35afc6c35c9dcb6e0ac4f236b142b1596bff
andrewmamych <andrew.mamych@gmail.com>
2021-04-29 02:01:59 +0300

    add greeting.js

---
+++ src/greeting.js
@@ -0,0 +1,10 @@
+import ask from './cli.js';
+
+const sayHello = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = ask('May I have your name? ');
+  console.log(`Hello, ${userName}!\n`);
+  return userName;
+};
+
+export default sayHello;
